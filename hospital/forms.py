8bfb5f1506048a1de25ece9bf6a804f27592cfa7
Abhinav Jaiswal <abhinavjaiswal1599@gmail.com>
2022-01-13 18:40:43 +0530

    Patient Summary

--- hospital/forms.py
+++ hospital/forms.py
@@ -3,30 +3,41 @@ from django.contrib.auth.models import User
 from . import models
 
 # Doctor form
+
+
 class DoctorUserForm(forms.ModelForm):
     class Meta:
-        model=User
-        fields=['first_name','last_name','username','password']
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password']
         widgets = {
-        'password': forms.PasswordInput()
+            'password': forms.PasswordInput()
         }
+
+
 class DoctorForm(forms.ModelForm):
     class Meta:
-        model=models.Doctor
-        fields=['address','mobile','department','profile_pic']
-#Patient
+        model = models.Doctor
+        fields = ['address', 'mobile', 'department', 'profile_pic']
+# Patient
+
+
 class PatientUserForm(forms.ModelForm):
     class Meta:
-        model=User
-        fields=['first_name','last_name','username','password']
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password']
         widgets = {
-        'password': forms.PasswordInput()
+            'password': forms.PasswordInput()
         }
+
+
 class PatientForm(forms.ModelForm):
-    #this is the extrafield for linking patient and their assigend doctor
-    #this will show dropdown __str__ method doctor model is shown on html so override it
-    #to_field_name this will fetch corresponding value  user_id present in Doctor model and return it
-    assignedDoctorId=forms.ModelChoiceField(queryset=models.Doctor.objects.all().filter(status=True),empty_label="Name and Department", to_field_name="user_id")
+    # this is the extrafield for linking patient and their assigend doctor
+    # this will show dropdown __str__ method doctor model is shown on html so override it
+    # to_field_name this will fetch corresponding value  user_id present in Doctor model and return it
+    assignedDoctorId = forms.ModelChoiceField(queryset=models.Doctor.objects.all(
+    ), empty_label="Name and Department", to_field_name="user_id")
+
     class Meta:
-        model=models.Patient
-        fields=['address','mobile','symptoms','profile_pic']
+        model = models.Patient
+        fields = ['address', 'mobile', 'symptoms', 'profile_pic', 'allergy_substance', 'allergy_criticality',
+                  'problems_list', 'immunization', 'history_procedure', 'diagnostic_results', 'past_history_of_illness']
--- hospital/models.py
+++ hospital/models.py
@@ -3,43 +3,67 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-departments=[('Cardiologist','Cardiologist'),
-('Dermatologists','Dermatologists'),
-('Emergency Medicine Specialists','Emergency Medicine Specialists'),
-('Allergists/Immunologists','Allergists/Immunologists'),
-('Anesthesiologists','Anesthesiologists'),
-('Colon and Rectal Surgeons','Colon and Rectal Surgeons')
-]
+departments = [('Cardiologist', 'Cardiologist'),
+               ('Dermatologists', 'Dermatologists'),
+               ('Emergency Medicine Specialists',
+                'Emergency Medicine Specialists'),
+               ('Allergists/Immunologists', 'Allergists/Immunologists'),
+               ('Anesthesiologists', 'Anesthesiologists'),
+               ('Colon and Rectal Surgeons', 'Colon and Rectal Surgeons')
+               ]
+
+
 class Doctor(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic= models.ImageField(upload_to='profile_pic/DoctorProfilePic/',null=True,blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(
+        upload_to='profile_pic/DoctorProfilePic/', null=True, blank=True)
     address = models.CharField(max_length=40)
-    mobile = models.CharField(max_length=20,null=True)
-    department= models.CharField(max_length=50,choices=departments,default='Cardiologist')
+    mobile = models.CharField(max_length=20, null=True)
+    department = models.CharField(
+        max_length=50, choices=departments, default='Cardiologist')
+
     @property
     def get_name(self):
         return self.user.first_name+" "+self.user.last_name
+
     @property
     def get_id(self):
         return self.user.id
+
     def __str__(self):
-        return "{} ({})".format(self.user.first_name,self.department)
+        return "{} ({})".format(self.user.first_name, self.department)
+
+
+criticality = [('low', 'low'), ('intermediate',
+                                'intermediate'), ('high', 'high')]
+
 
 class Patient(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    profile_pic= models.ImageField(upload_to='profile_pic/PatientProfilePic/',null=True,blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(
+        upload_to='profile_pic/PatientProfilePic/', null=True, blank=True)
     address = models.CharField(max_length=40)
-    mobile = models.CharField(max_length=20,null=False)
-    
-    symptoms = models.CharField(max_length=100,null=False)
+    mobile = models.CharField(max_length=20, null=False)
+
+    symptoms = models.CharField(max_length=100, null=False)
     assignedDoctorId = models.PositiveIntegerField(null=True)
-    admitDate=models.DateField(auto_now=True)
-    
+    allergy_substance = models.CharField(max_length=100)
+    allergy_criticality = models.CharField(
+        max_length=20, choices=criticality, default='low')
+    problems_list = models.CharField(max_length=100, null=False)
+    immunization = models.CharField(max_length=100, null=False)
+    history_procedure = models.CharField(max_length=100, null=False)
+    diagnostic_results = models.FileField(
+        upload_to='patient_document/', null=True, blank=True)
+    past_history_of_illness = models.CharField(max_length=100, null=False)
+
     @property
     def get_name(self):
         return self.user.first_name+" "+self.user.last_name
+
     @property
     def get_id(self):
         return self.user.id
+
     def __str__(self):
         return self.user.first_name+" ("+self.symptoms+")"
