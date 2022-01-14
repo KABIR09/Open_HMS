379840965945d5c6fa57b41fedff6deb9cf7f704
Kabir Roop <kabir.roop@innovaccer.com>
2022-01-14 18:31:58 +0530

    Add admin prescription

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Doctor, Patient
+from .models import Doctor, Patient, PatientPrescriptionDetails
 # Register your models here.
 class DoctorAdmin(admin.ModelAdmin):
     pass
@@ -11,3 +11,8 @@ admin.site.register(Doctor, DoctorAdmin)
 class PatientAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Patient, PatientAdmin)
+
+class PatientPrescriptionDetailsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(PatientPrescriptionDetails, PatientPrescriptionDetailsAdmin)
